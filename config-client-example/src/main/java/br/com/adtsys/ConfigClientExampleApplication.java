ec1aaeee9dec8725c674b3c89aa78f152687604a
Lucas Gertel <lgertel@gmail.com>
2017-06-20 10:41:23 -0300

    client example for developers

---
+++ config-client-example/src/main/java/br/com/adtsys/ConfigClientExampleApplication.java
@@ -0,0 +1,12 @@
+package br.com.adtsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientExampleApplication.class, args);
+	}
+}
